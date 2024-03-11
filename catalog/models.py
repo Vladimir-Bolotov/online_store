b15318bdb2a91016e4c6584a63dbcf46e512dd3e
Vladimir-Bolotov <mory4ok.89@yandex.ru>
2024-03-11 15:09:15 +0500

    add migrate catalog.0002_product_manufactured_at.

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Наименование")
+    description = models.TextField(verbose_name="Описание")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Наименование")
+    description = models.TextField(verbose_name="Описание")
+    image = models.ImageField(**NULLABLE, verbose_name="Изображение")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="Категория")
+    price = models.IntegerField(verbose_name="Цена")
+    created_at = models.DateField(verbose_name="Дата создания")
+    updated_at = models.DateField(verbose_name="Дата последнего изменения")
+    manufactured_at = models.DateField(**NULLABLE, verbose_name="Дата производства продукта")
+
+    def __str__(self):
+        return f"{self.name, self.price}"
+
+    class Meta:
+        verbose_name = 'товар'
+        verbose_name_plural = 'товары'
